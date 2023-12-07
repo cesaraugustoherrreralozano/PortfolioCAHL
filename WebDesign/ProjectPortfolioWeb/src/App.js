ea41f159980fbfa23d00e98e5711caca3e716ed1
INGNRGF <81452973+cesaraugustoherrreralozano@users.noreply.github.com>
2023-12-07 16:46:01 -0500

    Update

--- WebDesign/ProjectPortfolioWeb/src/App.js
+++ WebDesign/ProjectPortfolioWeb/src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes, useLocation} from 'react-router-dom'
 import store from './store';
 import {Provider} from 'react-redux'
 import Error404 from './containers/errors/Error404';
@@ -8,27 +8,46 @@ import Services from './containers/pages/Services';
 import About from './containers/pages/About';
 import Blog from './containers/pages/Blog';
 import Hire from './containers/pages/Hire';
+import {Helmet, HelmetProvider} from 'react-helmet-async'
+import AnimatedRoutes from './AnimatedRoutes';
  
 
 
 function App() {
   return (
-    <Provider store={store}>
-        <Router>
-      <Routes>
-        {/* Error Display */}
-        <Route path="*" element={<Error404/>} />
+    <HelmetProvider>
+      <Helmet>
+        <title>CAHL | Analytics</title>
+        <meta name='description' content='We transform data into valuable insights to enhance your decision-making. Discover how CAHL Analytics can drive the success of your business.' />
+        <meta name='keywords' content='data analysis, data transformation, insights, decision-making,business analytics, data analytics, data science, business intelligence, boosting business success, web design, CAHL analytics'/>
+        <meta name='robots' content='all'/>
+        <meta name='author' content='CAHL analytics'/>
+        <link rel='canonical'  href=''/>
 
-        {/* Home Display */}
-        <Route path='/' element={<Home/>}/>
-        <Route path='/Portfolio' element={<Portfolio/>}/>
-        <Route path='/Services' element={<Services/>}/>
-        <Route path='/About' element={<About/>}/>
-        <Route path='/Blog' element={<Blog/>}/>
-        <Route path='/Hire' element={<Hire/>}/>
-      </Routes>
-    </Router>
-    </Provider>
+        {/* Social Media Tags */}
+        <meta property="og:title" content='CAHL | Analytics' />
+        <meta property="og:description" content='We transform data into valuable insights to enhance your decision-making. Discover how CAHL Analytics can drive the success of your business.' />
+        <meta property="og:url" content="https://www.murkiva.com/" />
+        <meta property="og:image" content='https://bafkreienke2lhx43yekxqhazlhsa7x3yo2rtrapnuk3hzofbmuqnx27sea.ipfs.w3s.link' />
+
+        <meta name="twitter:title" content='CAHL | Analytics' />
+        <meta
+            name="twitter:description"
+            content='We transform data into valuable insights to enhance your decision-making. Discover how CAHL Analytics can drive the success of your business.'
+        />
+        <meta name="twitter:image" content='https://bafkreienke2lhx43yekxqhazlhsa7x3yo2rtrapnuk3hzofbmuqnx27sea.ipfs.w3s.link' />
+        <meta name="twitter:card" content="summary_large_image" />
+
+
+      </Helmet>
+        <Provider store={store}>
+            <Router>
+               <AnimatedRoutes/>
+            </Router>
+        </Provider>
+      
+    </HelmetProvider>
+
     
   );
 }
--- WebDesign/ProjectPortfolioWeb/src/hocs/layouts/layout.js
+++ WebDesign/ProjectPortfolioWeb/src/hocs/layouts/layout.js
@@ -1,10 +1,17 @@
 import {connect} from "react-redux"
+import {motion} from 'framer-motion'
 
 function Layout({children}){
     return(
-        <div>
+        <motion.div
+        
+            initial={{opacity:0, transition:{duration:0.5}}}
+            animate={{opacity:1, transition:{duration:0.5}}}
+            exit={{opacity:0, transition:{duration:0.5}}}
+        
+        >
             {children}
-        </div>
+        </motion.div>
     )
 }
 
